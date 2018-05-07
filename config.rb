562fc3abe4bffc64c76680abae40a2bd2ca19dea
Gianluca Monti <gm3871@gmail.com>
2018-05-07 18:22:46 +0200

    fix path assets

--- config.rb
+++ config.rb
@@ -19,11 +19,12 @@ javascripts_dir = "js"
 
 preferred_syntax = :scss
 
-css_dir = "css" # by Compass.app
-sass_dir = "scss" # by Compass.app
-images_dir = "images" # by Compass.app
-output_style = :compressed # by Compass.app
-relative_assets = true # by Compass.app
-line_comments = false # by Compass.app
-sass_options = {:debug_info=>false} # by Compass.app
-sourcemap = true # by Compass.app
+
+css_dir = "css" # by Compass.app 
+sass_dir = "scss" # by Compass.app 
+images_dir = "images" # by Compass.app 
+output_style = :expanded # by Compass.app 
+relative_assets = false # by Compass.app 
+line_comments = false # by Compass.app 
+sass_options = {:debug_info=>false} # by Compass.app 
+sourcemap = true # by Compass.app 
