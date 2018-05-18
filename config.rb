19502c0c120d9e5c7fe8c3de266e122c513991b4
gianluca monti <gm3871@gmail.com>
2018-05-18 16:32:09 +0200

    ...

--- config.rb
+++ config.rb
@@ -20,10 +20,10 @@ javascripts_dir = "js"
 preferred_syntax = :scss
 
 
-css_dir = "css" # by Compass.app 
-sass_dir = "scss" # by Compass.app 
-images_dir = "images" # by Compass.app 
-output_style = :expanded # by Compass.app 
+css_dir = "Assets/css" # by Compass.app 
+sass_dir = "Assets/scss" # by Compass.app 
+images_dir = "Assets/images" # by Compass.app 
+output_style = :compact # by Compass.app 
 relative_assets = false # by Compass.app 
 line_comments = false # by Compass.app 
 sass_options = {:debug_info=>false} # by Compass.app 
